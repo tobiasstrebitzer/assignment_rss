407b5bb587f39dca45d13054b4530e55f96d8062
Reydi Sutandang <reydi.sutandang@yahoo.com>
2014-07-14 14:35:55 +0800

    should initialize a data source

--- lib/assignment_rss/source.rb
+++ lib/assignment_rss/source.rb
@@ -1,7 +1,31 @@
+require 'rss'
+
 module AssignmentRss
   class Source
 
     # This is where the party starts
-    
+    def initialize(*args)
+    	@url, @max_age, @feed_class, @entry_class  = args
+    end
+
+    def url
+    	@url
+    end
+
+    def max_age
+    	@max_age
+    end
+
+    def feed_class
+    	@feed_class
+    end
+
+    def entry_class
+    	@entry_class
+    end
+
+    def get_feed
+    	RSS::
+
   end
 end
