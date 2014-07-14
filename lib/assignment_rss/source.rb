8fce94539ef89e401ba676281b7f29b3fbe478ca
Reydi Sutandang <reydi.sutandang@yahoo.com>
2014-07-14 16:54:34 +0800

    create attr_reader, refactor get_feed methods

--- lib/assignment_rss/source.rb
+++ lib/assignment_rss/source.rb
@@ -2,30 +2,55 @@ require 'rss'
 
 module AssignmentRss
   class Source
+  	attr_reader :url, :max_age, :feed_class, :entry_class, :title, :rss_feed_entries
+  	# attr_accessor :title, :rss_feed_entries
 
     # This is where the party starts
     def initialize(*args)
-    	@url, @max_age, @feed_class, @entry_class  = args
-    end
-
-    def url
-    	@url
-    end
-
-    def max_age
-    	@max_age
-    end
-
-    def feed_class
-    	@feed_class
-    end
-
-    def entry_class
-    	@entry_class
+    	@url, @max_age, @feed_class, @entry_class, @title, @rss_feed_entries = args
     end
 
     def get_feed
-    	RSS::
+    	open(@url) do |rss|
+    		feed = RSS::Parser.parse(rss)
+    		# @title = feed.entry.title
+    		rss_feed_enties = feed.entry
+    	end
+    	cached = true
+    	create_feed(feed)
+    	rss_feed_entries.each do |rfe|
+	    	create_feed_entries(rfe)
+    	end
+    end
+
+    def cached
+    	false
+    end
+
+    def create_feed(feed)
+    	@feed = RssFeed.new do |rf|
+    		rf.feed_id = feed.id
+    		rf.url = @url
+    		rf.title = feed.title
+    		rf.link = feed.link
+    		rf.rss_updated = feed.updated
+    		rf.cache = true
+    	end
+    	@feed.save
+    end
+
+    def create_feed_entries(entries)
+    	@rss_feed_entries = RssFeedEntry.new do |re|
+		re.entry_id = entries.id
+		re.title = entries.title
+		re.author = entries.author.name
+		re.link = entries.link
+		re.rss_updated = entries
+		re.content = entries.content
+		re.rss_feed_id = entries
+	  end
+    	@rss_feed_entries.save
+    end
 
   end
 end
--- spec/assignment_rss_source_spec.rb
+++ spec/assignment_rss_source_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 # This test suite provides a briefing of what needs to be implemented.
 #
-# General Description: 
+# General Description:
 #   We want to create a gem that abstracts an RSS-Feed (with its entries) and save them as an
-#   ActiveRecord object into the database. By doing so, we are able to (1) only download a feed once in the set cache lifetime, 
+#   ActiveRecord object into the database. By doing so, we are able to (1) only download a feed once in the set cache lifetime,
 #   and (2) also will lateron be able to use the classic rails active-record logic to e.g. fetch only specific entries of an
 #   rss feed, search through all feed entries, etc...
 #
@@ -25,18 +25,18 @@ require 'spec_helper'
 #
 
 # Case 1
-# test_feed_url = 'http://weblog.rubyonrails.org/feed/atom.xml'
-# test_feed_title = 'Riding Rails'
+test_feed_url = 'http://weblog.rubyonrails.org/feed/atom.xml'
+test_feed_title = 'Riding Rails'
 
 # Case 2
-test_feed_url = 'http://localhost/rss/botd.wordpress.com'
-test_feed_title = 'WordPress.com Top Posts'
+#test_feed_url = 'http://localhost/rss/botd.wordpress.com'
+#test_feed_title = 'WordPress.com Top Posts'
 
 
 describe AssignmentRss::Source do
-  
+
   # Upon instantiation of a AssignmentRss::Source, all configuration options
-  # (url, cache-lifetime, ActiveRecord-rss-feed-class and ActiveRecord-rss-feed-entry-class) 
+  # (url, cache-lifetime, ActiveRecord-rss-feed-class and ActiveRecord-rss-feed-entry-class)
   # should be stored in the source object.
   # The source should not automatically start downloading any data.
   it 'should initialize a data source' do
@@ -65,19 +65,19 @@ describe AssignmentRss::Source do
   # records from the database (cached) and return the result. The 'cached' attribute of the data source indicates if
   # the get_feed result will be received from cache (true), or downloaded from the RSS-Feed source (false).
   it 'should get a cached result when run the second time' do
-    
+
     # The first result will be downloaded from the feed url (uncached)
     source1 = AssignmentRss::Source.new(test_feed_url, 10.minutes, RssFeed, RssFeedEntry)
     source1.cached.should === false
     feed1 = source1.get_feed
     feed1.title.should == test_feed_title
-    
+
     # The second result will be fetched from the database (cached)
     source2 = AssignmentRss::Source.new(test_feed_url, 10.minutes, RssFeed, RssFeedEntry)
     source2.cached.should === true
     feed2 = source2.get_feed
     feed2.title.should == test_feed_title
-    
+
   end
 
   # The activerecord- objects should have valid attributes (at least a title).
@@ -87,11 +87,11 @@ describe AssignmentRss::Source do
     source.cached.should === false
     feed = source.get_feed
     feed.title.should == test_feed_title
-    
+
     entries = feed.rss_feed_entries
     entries.length.should > 0
     entries[0].title.class.should == String
 
   end
- 
+
 end
