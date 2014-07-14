7757707fbd8185bb875e351236ed71f21f38349d
Reydi Sutandang <reydi.sutandang@yahoo.com>
2014-07-14 18:42:45 +0800

    using simpleRSS, passed 2nd test

--- lib/assignment_rss/source.rb
+++ lib/assignment_rss/source.rb
@@ -1,4 +1,5 @@
-require 'rss'
+require 'simple-rss'
+require 'open-uri'
 
 module AssignmentRss
   class Source
@@ -7,47 +8,55 @@ module AssignmentRss
 
     # This is where the party starts
     def initialize(*args)
-    	@url, @max_age, @feed_class, @entry_class, @title, @rss_feed_entries = args
+    	@url, @max_age, @feed_class, @entry_class, @title, @rss_feed_entries, @cache = args
     end
 
     def get_feed
+    	# @feed = RssFeed.find_by_url(@url)
+    	# if @feed.nil?
     	open(@url) do |rss|
-    		feed = RSS::Parser.parse(rss)
+    		feed = SimpleRSS.parse(rss)
     		# @title = feed.entry.title
-    		rss_feed_enties = feed.entry
-    	end
-    	cached = true
-    	create_feed(feed)
-    	rss_feed_entries.each do |rfe|
-	    	create_feed_entries(rfe)
-    	end
+    		rss_feed_entries = feed.feed.entries
+    		@cache = true
+    		create_feed(feed, @url)
+                                rss_feed_entries.each do |rfe|
+	    		create_feed_entries(rfe, feed.id)
+                                end
+	    	end
+
+	    	feed = RssFeed.find_by_url(@url)
+  #   	else
+		# @feed
+		# @cache = @feed.cache
+  #   	end
     end
 
     def cached
-    	false
+    	@cache ||= false
     end
 
-    def create_feed(feed)
+    def create_feed(feed, url)
     	@feed = RssFeed.new do |rf|
-    		rf.feed_id = feed.id
-    		rf.url = @url
-    		rf.title = feed.title
-    		rf.link = feed.link
-    		rf.rss_updated = feed.updated
+    		rf.feed_id = feed.id.to_s
+    		rf.url = url.to_s
+    		rf.title = feed.title.to_s
+    		rf.link = feed.link.to_s
+    		rf.rss_updated = Time.parse(feed.updated.to_s).to_datetime
     		rf.cache = true
     	end
     	@feed.save
     end
 
-    def create_feed_entries(entries)
+    def create_feed_entries(entries, feed_id)
     	@rss_feed_entries = RssFeedEntry.new do |re|
-		re.entry_id = entries.id
-		re.title = entries.title
-		re.author = entries.author.name
-		re.link = entries.link
-		re.rss_updated = entries
-		re.content = entries.content
-		re.rss_feed_id = entries
+		re.entry_id = entries.id.to_s
+		re.title = entries.title.to_s
+		re.author = entries.author.to_s
+		re.link = entries.link.to_s
+		re.rss_updated = DateTime.parse(entries.updated.to_s).to_datetime
+		re.content = entries.content.to_s
+		re.rss_feed_id = feed_id
 	  end
     	@rss_feed_entries.save
     end
